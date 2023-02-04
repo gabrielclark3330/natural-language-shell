80ba0d7ace12364c0d6ea80f30d9b63a0117afc4
rorth3 <rorth3@huskers.unl.edu>
2023-02-04 20:54:21 +0000

    push random junk

--- backend/test.cpp
+++ backend/test.cpp
@@ -4,14 +4,14 @@
 
 int main(void)
 {
-  httplib::Client cli("https://api.ipify.org?format=json");
+	httplib::Client cli("localhost", 1234);
 
-  if (auto res = cli.Get("")) {
-    if (res->status == 200) {
-      std::cout << res->body << std::endl;
-    }
-  } else {
-    auto err = res.error();
-    std::cout << "HTTP error: " << httplib::to_string(err) << std::endl;
-  }
+	if (auto res = cli.Get("/hi")) {
+    	if (res->status == 200) {
+			std::cout << res->body << std::endl;
+		}
+	} else {
+		auto err = res.error();
+		std::cout << "HTTP error: " << httplib::to_string(err) << std::endl;
+  	}
 }
