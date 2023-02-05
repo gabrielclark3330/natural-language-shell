8ab9d470c89679f7a1060c06e5cb6eab107429a3
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2023-02-05 09:14:40 -0600

    After stash changes

--- frontend/natural_language_shell/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/natural_language_shell/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
 }
