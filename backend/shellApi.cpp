eb470c1c3c66c3bbae8398350dc332cd2944ad21
rorth3 <rorth3@huskers.unl.edu>
2023-02-04 23:07:08 -0600

    Got http Post working with OpenAI Create function for prompt entry and give response with http calls #15

---
+++ backend/shellApi.cpp
@@ -0,0 +1,67 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "cpp-httplib-master/httplib.h"
+#include <iostream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+httplib::Client client("https://api.openai.com");
+
+std::string getToken(){
+	std::ifstream f("secret.json");
+	json data = json::parse(f);
+	return data["OpenAIKey"];
+}
+
+std::string sendPrompt(std::string prompt){
+
+	json data = {
+		{"model", "code-davinci-002"},
+		{"prompt", "/*" + prompt + "*/"},
+		{"max_tokens", 1024},
+		// {"temperature", 1},
+		// {"top_p", 1},
+		// {"n", 1},
+		// {"stream", false},
+		// {"logprobs", NULL},
+		// {"echo", false},
+		// {"stop", NULL},
+		// {"presence_penalty", 0},
+		// {"frequency_penalty", 0},
+		// {"best_of", 1},
+		// {"logit_bias", NULL},
+		// {"user", NULL}
+	};
+
+	std::string result = "";
+
+	auto res = client.Post("/v1/completions", data.dump(), "application/json");
+
+	if (res && res->status == 200) {
+		std::cout << "Successful response" << std::endl;
+		// std::cout << "Successful response: " << res->body << std::endl;
+		
+		json response = json::parse(res->body);
+    	std::vector<json> choices = response["choices"];
+    	result = choices[0]["text"];
+
+	} else {
+		std::cout << "Request failed" << std::endl;
+		std::cout << res->body << std::endl;
+	}
+
+	return result;
+}
+
+int main() {
+
+	std::string prompt = "Create a python program to print hello world";
+
+	client.set_bearer_token_auth(getToken());
+
+	std::string response = sendPrompt(prompt);
+
+	std::cout << response << std::endl;
+
+	return 0;
+}
